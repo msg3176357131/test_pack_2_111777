cd3c7dcd627191d2925ecf957c504eb60b59c49c
user <user@localhost.localdomain>
2025-12-11 19:27:16 +0530

    init

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = "simple utility utility"
+
+setup(
+    name="test_pack_111777",
+    version="1.0.0",
+    author="Lora Bora",
+    author_email="lorabora111777111777@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: Unix",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    keywords=[
+        "python",
+        "console",
+    ],
+    python_requires=">=3.7",
+)
+
---
+++ test_pack_2_111777/__init__.py
@@ -0,0 +1,8 @@
+from .main2 import test_main2
+
+__version__ = "1.0.0"
+__author__ = "Lora Bora"
+__email__ = "lorabora111777111777@gmail.com"
+
+__all__ = ["test_main2"]
+
